80a2489a941fc8c863cdd4d95a057dfccfac3d88
Melford-D <melfordbirakor@gmail.com>
2021-07-26 18:47:53 +0100

    added functionality for email

--- js/custom.js
+++ js/custom.js
@@ -26,3 +26,43 @@
   });
 
 })(jQuery); // End of use strict
+
+// Filter and Overlay
+$(document).ready(function(){
+
+$(".filter-b").click(function(){
+    var value = $(this).attr('data-filter');
+    if(value == "all")
+    { 
+        $('.filter').show('1000');
+    }
+    else
+    { 
+        $(".filter").not('.'+value).hide('3000');
+        $('.filter').filter('.'+value).show('3000');
+    }
+});
+
+if ($(".filter-b").removeClass("active")) {
+    $(this).removeClass("active");
+}
+$(this).addClass("active");
+});
+
+// SKILLS
+$(function () {
+    $('.counter').counterUp({
+        delay: 10,
+        time: 2000
+    });
+
+});
+
+// My Info
+function on() {
+  document.getElementById("overlay").style.display = "block";
+}
+
+function off() {
+  document.getElementById("overlay").style.display = "none";
+}
